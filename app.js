9a2153855d7018a78d1e7c31c63994b350516d70
Virgílio César <enjels@outlook.com>
2024-03-07 12:54:02 -0300

    #

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-const express = require("express")
+const express = require("express");
+
+const app = express();
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Minha lista de tarefas</h1>");
+});
+
+app.get("/json", (req, res) => {
+  console.log(req.body);
+  res.json({ title: "Tarefa X", done: true });
+});
+
+app.listen(3000, () => {
+  console.log("Servidor foi iníciado");
+});
